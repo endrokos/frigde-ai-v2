fb32248cc91cd7c79f7c7112303d3fe373fd500a
Daniel R <drestrepo@foqum.io>
2025-06-16 20:06:16 +0200

    fixed

--- back/src/recipes/domain/recipes_request.py
+++ back/src/recipes/domain/recipes_request.py
@@ -3,10 +3,7 @@ from pydantic import BaseModel, Field
 
 class RecipesRequest(BaseModel):
     plato: str
-    calories: int = Field(alias="calorias")
-    protein: int = Field(alias="proteinas")
-    carbohydrate: int = Field(alias="hidratos")
-    fat: int = Field(alias="grasas")
-
-    class Config:
-        allow_population_by_field_name = True
+    calories: int
+    protein: int
+    carbohydrate: int
+    fat: int
